b19caf5684b6b77f4e7da8f2420bdc2b577a67d7
Josh Allard <pokeystampyfan@gmail.com>
2025-02-11 16:12:23 +0000

    ok so its getting the map number right but some problem

--- client.py
+++ client.py
@@ -28,27 +28,22 @@ class Map():
 
         wood_block_image = pygame.image.load('0.png')
         dirt_path_image = pygame.image.load('1.png')
-        row = 0
-        for row in data:
-            column = 0
-            for tile in row:
+        for row_index, row in enumerate(data):
+            for column_index, tile in enumerate(row):
                 if tile == 0:
                     img = pygame.transform.scale(wood_block_image, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = column * tile_size
-                    img_rect.y = row * tile_size
+                    img_rect.x = column_index * tile_size
+                    img_rect.y = row_index * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                 if tile == 1:
                     img = pygame.transform.scale(dirt_path_image, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = column * tile_size
-                    img_rect.y = row * tile_size
+                    img_rect.x = column_index * tile_size
+                    img_rect.y = row_index * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
-
-                column += 1
-            row += 1
 
     def draw(self):
         for tile in self.tile_list:
