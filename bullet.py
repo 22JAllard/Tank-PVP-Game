811735cc6be10344fdbcd13368518513a6a53a1e
Josh Allard <22JAllard@tgschool.net>
2025-03-26 17:29:20 +0000

    wip3

--- bullet.py
+++ bullet.py
@@ -40,10 +40,10 @@ class Bullet:
         self.last_redraw_time = time.time()
 
 
-        # factored_speed = self.delta_time / self.bullet_speed
-        factored_speed = self.bullet_speed
-        # factored_diagonal_speed = self.delta_time / self.bullet_speed
-        factored_diagonal_speed =  self.bullet_speed
+        factored_speed = self.delta_time / self.bullet_speed
+        # factored_speed = self.bullet_speed
+        factored_diagonal_speed = self.delta_time / self.bullet_speed
+        # factored_diagonal_speed =  self.bullet_speed
         #print("welcome to bullet move")
         if self.angle == 0: 
             dy = factored_speed #self.bullet_speed
