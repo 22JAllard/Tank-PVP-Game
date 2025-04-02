c916e6f9479d70b3fb72dfda2367418a91093eb1
JoshA0210 <22JAllard@tgschool.net>
2025-04-02 10:57:18 +0100

    did some of network

--- network.py
+++ network.py
@@ -3,34 +3,31 @@ import pickle
 
 class Network:
     def __init__ (self, server_ip, client_colour, scale):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create a TCP socket for the client (AF_INET = IPv4, SOCK_STREAM = TCP)
         self.server = server_ip #ip address of server
         print("Connecting to server: ", self.server)
-        self.port = 5555
-        self.addr = (self.server, self.port)
-        self.initial_data = self.connect(client_colour, scale)
+        self.port = 5555 #same port as the server is using
+        self.addr = (self.server, self.port) #create a tuple with the server ip and port
+        self.initial_data = self.connect(client_colour, scale) #call the class' connect function with colour and scale
 
     def connect (self, client_colour, scale):
         try:
-            self.client.connect(self.addr)
+            self.client.connect(self.addr) #connect to the IP and port
             self.client.settimeout(10.0)  #10 second timeout
-            self.connected = True
+            self.connected = True #if the network is able to connect
             print("Connected successfully")
 
-            
-            # Send client colour
-            colour_and_scale = (client_colour, scale)
-            self.client.send(pickle.dumps(colour_and_scale)) 
-            
-            # Receive initial data
-            data = self.client.recv(4096)
-            if not data:
+            colour_and_scale = (client_colour, scale) #create a tuple with the colour and scale
+            self.client.send(pickle.dumps(colour_and_scale)) #send the colour and scale to server, serialised
+
+            data = self.client.recv(4096) #receieve data from the server
+            if not data: #if there is no data 
                 print("No initial data received")
                 return False
 
-            print("initial data", pickle.loads(data))
-            return pickle.loads(data)
-        except socket.timeout:
+            return pickle.loads(data) #returns the data which was received
+        
+        except socket.timeout: #if there is an error/timeout, disconnect 
             print("Connection timed out")
             self.disconnect()
             return False
@@ -39,14 +36,15 @@ class Network:
             self.disconnect()
             return False
             
-    def receive_map_number(self):
+    def receive_map_number(self): 
         try:
-            data = self.client.recv(8192)
+            data = self.client.recv(8192) 
             if data:
                 return pickle.loads(data)
             else:
                 print("No data received from server")
                 return None
+            
         except Exception as e:
             print("Error receiving map number:", e)
             return None
@@ -64,8 +62,8 @@ class Network:
                 print("No data received from server")
                 self.disconnect()
                 return None
-  
             return pickle.loads(received_data) #contains both tanks and bullets
+        
         except socket.timeout:
             print("Send/receive timed out")
             self.disconnect()
@@ -79,7 +77,6 @@ class Network:
             self.disconnect()
             return None
 
-        
     def disconnect(self):
         self.connected = False
         try:
